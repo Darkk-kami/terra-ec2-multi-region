98491918577b2ae9ef2179f2a5ddaa7191719a14
Darkk-kami <raw.dwayne@gmail.com>
2024-09-04 13:05:54 +0100

    added regions

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 module "region_1" {
   source = "./region"
   region = "us-east-1"
@@ -9,9 +8,39 @@ module "region_2" {
   region = "us-east-2"
 }
 
+module "region_3" {
+  source = "./region"
+  region = "us-west-1"
+}
+
+module "region_4" {
+  source = "./region"
+  region = "us-west-2"
+}
+
+module "region_5" {
+  source = "./region"
+  region = "eu-west-1"
+}
+
+module "region_6" {
+  source = "./region"
+  region = "eu-west-2"
+}
+
+module "region_7" {
+  source = "./region"
+  region = "eu-west-3"
+}
+
 locals {
   modules = {
     "us-east-1" = module.region_1.servers
     "us-east-2" = module.region_2.servers
+    "us-west-1" = module.region_3.servers
+    "us-west-2" = module.region_4.servers
+    "eu-west-1" = module.region_5.servers
+    "eu-west-2" = module.region_5.servers
+    "eu-west-3" = module.region_5.servers
   }
 }
