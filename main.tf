668695a20479c60d2874cba73b8c24c688b3b159
Darkk-kami <71535947+Darkk-kami@users.noreply.github.com>
2024-09-04 13:09:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ locals {
     "us-west-1" = module.region_3.servers
     "us-west-2" = module.region_4.servers
     "eu-west-1" = module.region_5.servers
-    "eu-west-2" = module.region_5.servers
-    "eu-west-3" = module.region_5.servers
+    "eu-west-2" = module.region_6.servers
+    "eu-west-3" = module.region_7.servers
   }
 }
